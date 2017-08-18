979e0dd948ba10cb12d8a95ade6da427df8cedb4
Ruben de Laat <Ruben de Laat@LAPTOP-493FREB8.tsn.tno.nl>
2017-08-18 11:41:12 +0200

    Added read/modify/write test

---
+++ IfcPlugins/test/org/bimserver/test/TestReadModifyWrite.java
@@ -0,0 +1,55 @@
+package org.bimserver.test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Paths;
+
+import org.apache.commons.io.IOUtils;
+import org.bimserver.emf.IfcModelInterface;
+import org.bimserver.emf.PackageMetaData;
+import org.bimserver.emf.Schema;
+import org.bimserver.ifc.step.deserializer.Ifc2x3tc1StepDeserializer;
+import org.bimserver.ifc.step.serializer.Ifc2x3tc1StepSerializer;
+import org.bimserver.models.ifc2x3tc1.Ifc2x3tc1Package;
+import org.bimserver.models.ifc2x3tc1.IfcWall;
+import org.bimserver.plugins.PluginConfiguration;
+import org.bimserver.plugins.deserializers.DeserializeException;
+import org.bimserver.plugins.serializers.SerializerException;
+import org.junit.Test;
+
+public class TestReadModifyWrite {
+	@Test
+	public void test() {
+		Ifc2x3tc1StepDeserializer deserializer = new Ifc2x3tc1StepDeserializer();
+		PackageMetaData packageMetaData = new PackageMetaData(Ifc2x3tc1Package.eINSTANCE, Schema.IFC2X3TC1, Paths.get("tmp"));
+		deserializer.init(packageMetaData);
+		
+		try {
+			URL url = new URL("https://raw.githubusercontent.com/opensourceBIM/IFC-files/master/HHS%20Office/construction.ifc");
+			InputStream openStream = url.openStream();
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			IOUtils.copy(openStream, baos);
+			IfcModelInterface model = deserializer.read(new ByteArrayInputStream(baos.toByteArray()), "", baos.size(), null);
+			
+			for (IfcWall ifcWall : model.getAllWithSubTypes(IfcWall.class)) {
+				ifcWall.setName(ifcWall.getName() + " Modified");
+			}
+			
+			Ifc2x3tc1StepSerializer serializer = new Ifc2x3tc1StepSerializer(new PluginConfiguration());
+			serializer.init(model, null, false);
+			serializer.writeToFile(Paths.get("output.ifc"), null);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (DeserializeException e) {
+			e.printStackTrace();
+		} catch (SerializerException e) {
+			e.printStackTrace();
+		}
+	}
+}
